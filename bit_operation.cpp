7d7149d12e20984d0bdbb291f4561a8dc3967c02
Dongha Yoon <cs.donghayoon@gmail.com>
2020-09-25 14:27:09 +0900

    0925

--- bit_operation.cpp
+++ bit_operation.cpp
@@ -3,22 +3,22 @@
 using namespace std;
 
 int main(){
-    int x=0x00000000;
-    int y=0x7fffffff;
-    int z=0x80043000;
+    int x[4];
+    int Tmin = 1<<31;
+    int Tmax = ~Tmin;
 
+    for(int i=0;i<4;i++)
+        cin >> x[i];
+    for(int i=0;i<4;i++){
+        cout << hex<<"0x"<<( (~x[i]+1)^x[i] )<<" "<<"0x"<<( (~x[i]+1)&x[i] ); 
 
-    cout << "-x^x: " << ((-x^x)) <<endl;
-    cout << "-y^y: " << ((-y^y)) <<endl;
-    cout << "-z^z: " << ((-z^z)) <<endl;
-    cout << endl;
-    cout << "-x&x: " << ((-x&x)) <<endl;
-    cout << "-y&y: " << ((-y&y)) <<endl;
-    cout << "-z&z: " << ((-z&z)) <<endl;
-    cout << endl;
-    cout << "-x|x: " << ((-x|x)) <<endl;
-    cout << "-y|y: " << ((-y|y)) <<endl;
-    cout << "-z|z: " << ((-z|z)) <<endl;
-
-
+        cout << endl;
+    }
+    // for(int i=1;i<=32;i++){
+    //     cout << "int Tmin_"<<dec<<i<< " = 0x" << hex << (1<<(i-1))<<";" << endl;
+    // }
+    // for(int i=32;i>0;i--){
+    //     cout << "int bitVec_"<<dec<<i<<" = " << "(x&Tmin_"<<dec<<i<< ");" << "x = x>>bitVec_" << i<<";" << endl;
+    // }
 }
+
